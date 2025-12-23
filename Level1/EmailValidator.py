be06c59e2b9dae509dd2298a19f5bc88cd78be5a
TusharSuthaar <tushar26suthar@gmail.com>
2025-12-23 21:21:04 +0530

    email validator

---
+++ Level1/EmailValidator.py
@@ -0,0 +1,10 @@
+def emailvalidator(email):
+    import re
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+    
+result = input("Enter an email to validate: ")
+is_valid = emailvalidator(result)
